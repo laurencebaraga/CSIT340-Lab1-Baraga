d5547b345a51d323e0c816d60ec3dba57e4404ea
LAB24_USER ON WS08 <L24X15W08@LabHCI.net>
2026-09-14 17:48:16 +0800

    mana!

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,61 @@
-import './App.css'
-
 function App() {
-
   return (
-    <div className= "max-w-2xl mx-auto px-6 py-16" align="left">
+    <>
       <header className="max-w-2xl mx-auto px-6 pt-16">
-      <h1 className="text-4xl font-semibold tracking-tight">Laurence Andrey Baraga</h1>
-       <p className="mt-2 text-lg text-slate-600">Third year BSIT student at Cebu Institute of Technology – University.</p>
-      
-      <hr />
+        <h1 className="text-4xl font-semibold tracking-tight">Laurence Andrey Baraga</h1>
+        <p className="mt-2 text-lg text-slate-600">Third year BSIT student at Cebu Institute of Technology – University.</p>
+        <hr className="mt-8 border-slate-200" />
       </header>
-
-    <main className="mb-12 space-y-4">
-      <section className="mb-12 space-y-100">      
-      <h2 className="text-xl font-semibold mb-2">About</h2>
-      <p className="text-gray-700">
-        I grew up in Cebu City, Philippines. I've been in CIT-U for the last almost fifteen years,
-        I like making art, especially sketching and concept art. I play videogames like MLBB and HSR.
-        I'm currently in my third year of BSIT, and I want to pursue a career in game development.
+ 
+         <main className="max-w-2xl mx-auto px-6 py-10">
+ 
+            <section className="mb-10">
+              <h2 className="text-xl font-semibold mb-3">About</h2>
+              <p className="leading-relaxed text-slate-700">
+              I am 21 years old and I am currently a third year BSIT student at Cebu Institute of Technology - University.
+              I like drawing and making art. I also like collecting cards on my spare time. </p>
+            </section>
+ 
+               <section className="mb-10">
+      <h2 className="text-xl font-semibold mb-3">Details</h2>
+      <p className="leading-relaxed text-slate-700">
+        Course: BS Information Technology
+       </p>
+      <p className="leading-relaxed text-slate-700">
+         
+        Year level: Third year
+        </p>
+        <p className="leading-relaxed text-slate-700">
+        Hometown: Quiot, Pardo, Cebu City
       </p>
-      </section>
-
-      <section className="mb-12 space-y-4">
-      <h2 className="text-xl font-semibold mb-2">Details</h2>
-      <p className="leading-relaxed text-slate-700">
-        Course: BS Information Technology </p>
-      <p>Year level: Third year </p>
-      <p>Hometown: Quiot, Pardo, Cebu</p>
-      </section>
-
-      <section className="mb-12 space-y-4">
-       <h2 className="text-xl font-semibold mb-2">Things I like</h2>
-      <ul className="list-disc list-inside space-y-1 text-slate-700">
-        <li>Playing MLBB on free time</li>
-        <li>Collecting TCG cards and buying packs once a month</li>
-        <li>Working on photography</li>
+    </section>    
+    <section className="mb-10">
+     <h2 className="text-xl font-semibold mb-3">Things I like</h2>
+     <ul className="list-disc list-inside space-y-1 text-slate-700">
+        <li>Collecting tcg</li>
+        <li>Making art</li>
+        <li>PLaying video games</li>
       </ul>
-      </section>
-
-      <section className="mb-12 space-y-4">
-      <h2 className="text-xl font-semibold mb-2">Reach me</h2>
+    </section>
+ 
+    <section>
+      <h2 className="text-xl font-semibold mb-3">Reach me</h2>
       <p className="leading-relaxed text-slate-700">
-        laurenceandrey.baraga@cit.edu </p>  
-      <p>@laurenceandrey on most things</p>
-      </section>
+        laurenceandrey.baraga@cit.edu
+        </p>
+        <p className="leading-relaxed text-slate-700">
+        @laurenceandreybaraga at things
+      </p>
+    </section>
+ 
     </main>
-
-      <hr />
-      <footer class="max-w-2xl mx-auto px-6 pb-16">
-        Made for CSIT340.
-      </footer>
-    </div>
+   
+      <section className="max-w-2xl mx-auto px-6 pb-16">
+        <hr className="mb-6 border-slate-200" />
+        <p className="text-sm text-slate-500">Made for CSIT340.</p>
+      </section>
+    </>
   )
 }
-
+ 
 export default App
